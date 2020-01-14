cc309923d27400e98f939bc26216cd64dfb4271c
DPigeon <D_Pig@encs.concordia.ca>
2020-01-14 17:27:59 -0500

    Action bar menu on profile activity

--- app/src/main/java/com/example/utilisateur/assignment1/ProfileActivity.java
+++ app/src/main/java/com/example/utilisateur/assignment1/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.example.utilisateur.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 /*
@@ -13,10 +15,36 @@ import android.widget.EditText;
 public class ProfileActivity extends AppCompatActivity {
 
     protected EditText nameEditText;
+    protected EditText ageEditText;
+    protected EditText stIdEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        nameEditText = findViewById(R.id.nameEditText);
+        ageEditText = findViewById(R.id.ageEditText);
+        stIdEditText = findViewById(R.id.stIdEditText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
