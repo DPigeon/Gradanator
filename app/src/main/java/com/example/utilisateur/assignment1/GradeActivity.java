0f2e90742c35b540d52d91f663f29e0798926221
DPigeon <D_Pig@encs.concordia.ca>
2020-01-16 00:51:26 -0500

    ListView & Adapter Setup

--- app/src/main/java/com/example/utilisateur/assignment1/GradeActivity.java
+++ app/src/main/java/com/example/utilisateur/assignment1/GradeActivity.java
@@ -3,6 +3,8 @@ package com.example.utilisateur.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -13,10 +15,16 @@ import java.util.ArrayList;
 
 public class GradeActivity extends AppCompatActivity {
     ArrayList<Course> courses;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade);
+
+        // Setting up the list view with its adapter
+        adapter = new ArrayAdapter<Course>(this, R.layout.activity_grade, courses);
+        ListView listView = findViewById(R.id.gradeListView);
+        listView.setAdapter(adapter);
     }
 }
